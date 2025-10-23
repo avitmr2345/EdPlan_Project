ea218069da05c9ad7b8b5d8e9faedc202b6451b7
Avish <avitmr2345@gmail.com>
2025-10-23 10:35:59 +0530

    project initialized

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
